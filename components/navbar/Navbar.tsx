c75f43b95e6ff4237eb00574b0505ba927d18701
AirinCompSci <airincompsci@gmail.com>
2022-05-16 15:43:43 -0400

    Made the navbar more mobile friendly

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar: FC = () => {
 
   return (
     <div className="h-12 w-full bg-neutral-900 bg-opacity-60 sticky top-0 flex justify-around z-10">
-      <div className="flex justify-around items-center gap-10">
+      <div className="text-center text-sm md:text-md flex justify-around items-center gap-5 md:gap-10 mx-4 md:mx-0">
         <NavbarItems
           title="Home"
           source="/"
